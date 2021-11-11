d6c153c65903f42a235b64bac3fcc115e9becc4f
soumyo19 <fsoummya@gmail.com>
2021-11-11 04:53:32 +0000

    lemonkebab: Init Nad
    Initial NusantaraOS A12 bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_lemonkebab.mk
+    $(LOCAL_DIR)/nad_lemonkebab.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lemonkebab-user \
-    aosp_lemonkebab-userdebug \
-    aosp_lemonkebab-eng
+    nad_lemonkebab-user \
+    nad_lemonkebab-userdebug \
+    nad_lemonkebab-eng
